f0e1cf8c3db7370451f118281920dff1d629f073
marcos1242 <166352381+marcos1242@users.noreply.github.com>
2024-07-02 00:38:42 -0300

    Delete matplotlib, pandas y numpy.py

--- matplotlib, pandas y numpy.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-df = pd.DataFrame(np.random.randn(100, 5), columns=list('ABCDE'))
-df=df.cumsum() # Return cumulative sum over a DataFrame or Series axis
-df.plot()
-plt.show()
